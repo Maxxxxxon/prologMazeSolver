025c28933fb20a0ee23018a88253904baea3793e
Maxxxxxon <m.faleev@innopolis.university>
2021-03-05 15:56:53 +0300

    backtracking shortest path 5x5

--- code.pl
+++ code.pl
@@ -1,28 +1,28 @@
-covid([2, 6]).
-covid([4, 2]).
+:-ensure_loaded(utils).
 
-doctor([5, 1]).
-home([9, 9]).
-mask([1, 4]).
+covid([2, 5]).
+covid([4, 1]).
+
+doctor([6, 6]).
+home([4, 5]).
+mask([2, 1]).
+restrictions(5).
 
 adjacent_cells(Cell1, Cell2) :-
     Cell2 = [X2, Y2],
     Cell1 = [X1, Y1],
     (X2 is X1 + 1; X2 is X1; X2 is X1 - 1),
     (Y2 is Y1 + 1; Y2 is Y1; Y2 is Y1 - 1),
-    X2 > 0, X2 < 10,
-    Y2 > 0, Y2 < 10, 
+    restrictions(L),
+    X2 > 0, X2 =< L,
+    Y2 > 0, Y2 =< L, 
     Cell1 \= Cell2.
 
 covidAdjacentD1(X) :-
     covid(Y),
     adjacent_cells(Y, X).
 
-concatenate([], L, L).
-concatenate([X|L1], L2, [X|L3]) :-   %%% rewrite
-  concatenate(L1, L2, L3).
-
-covid_safe(Cell, CompletedPath):-
+is_covid_safe(Cell, CompletedPath):-
     (   
       (covid(Cell); covidAdjacentD1(Cell)),
       (doctor(X), member(X, CompletedPath); mask(Y), member(Y, CompletedPath))
@@ -34,13 +34,22 @@ covid_safe(Cell, CompletedPath):-
 
 path(X, X, _, []) :- true.
 path(From, To, CompletedPath, FuturePath):-
+    restrictions(L),
+    length(CompletedPath, Len),
+    Len =< L * 2,
     adjacent_cells(From, Next),
-    covid_safe(Next, CompletedPath),
+    is_covid_safe(Next, CompletedPath),
     not(member(Next, CompletedPath)),
     concatenate(CompletedPath, [Next], NewCompletedPath),
     path(Next, To, NewCompletedPath, FuturePath2),
     FuturePath = [Next|FuturePath2].
     
 
-main(Path) :-
-    path([1, 1], [9, 1], [[1, 1]], Path).
+find_some_way(Path) :-
+    home(Home),
+    path([1, 1], Home, [[1, 1]], Path).
+
+find_shortest_way(Path) :-
+    home(Home),
+    bagof(PathP, path([1, 1], Home, [[1, 1]], PathP), L),
+    find_shortest_list(L, Path).
--- utils.pl
+++ utils.pl
@@ -5,3 +5,24 @@ concatenate([X|L1], L2, [X|L3]) :-   %%% rewrite
 max(A, B, X):-
     (A > B, X is A);
     (A =< B, X is B).
+
+
+substitute(Value, NewValue, List, NewList):-
+    delete(List, Value, List2),
+    concatenate(List2, [NewValue], List).
+
+shortestListOfTwo([List1, List2], List):-
+  length(List1, L1),
+  length(List2, L2),
+  (
+    (L1 <  L2, List = List1);
+    (L1 >= L2, List = List2)
+  ).
+
+find_shortest_list(ListOfLists, List):-
+  shortestListOfTwo(ListOfLists, List).
+
+find_shortest_list(ListOfLists, List):-
+  ListOfLists = [List1|T],
+  find_shortest_list(T, List2),
+  shortestListOfTwo([List1, List2], List).
